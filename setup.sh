125ad6338a7cea77fa5dd6e78e2899a3ed62d051
yashkhanofficial <info.yashkhan@gmail.com>
2026-03-05 00:45:14 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+echo "[*] Updating System..."
 pkg update && pkg upgrade -y
-pkg install python git -y
+
+echo "[*] Installing Python and Dependencies..."
+pkg install python git tsu -y
+
+echo "[*] Installing Python Libraries..."
 pip install scapy
+
 chmod +x mitm_tool.py
-echo "Setup Complete! Ekhon run koren: python mitm_tool.py"
+echo "[+] Setup Complete! Use 'sudo python mitm_tool.py' to run."
